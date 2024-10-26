007e97946a5e1e99c6a04bb0f29808c6ec31c2a1
ChidaoDouyu <3114680870@qq.com>
2024-10-26 23:34:40 +0800

    调整页脚

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -26,7 +26,7 @@ export default hopeTheme({
   sidebar,
 
   // 页脚
-  footer: "默认页脚",
+  footer: "",
   displayFooter: true,
   license: "CC BY-NC-ND 4.0 ",
 
--- docs/index.md
+++ docs/index.md
@@ -39,5 +39,4 @@ bgImage: /bg.jpg
 #     desc: 自定义详细介绍
 #     link: https://你的自定义链接
 
-footer: 自定义你的页脚文字
 ---
