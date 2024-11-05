528adac8047d3663d3b707851499209a42ec6830
ChidaoDouyu <3114680870@qq.com>
2024-11-05 22:39:09 +0800

    修复顶栏bug - 目录结构更改

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,26 +3,19 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   {
-    text: "博文",
+    text: "高中文化课",
     icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "高中文化课",
-        icon: "pen-to-square",
-        prefix: "culture/",
-        children:[
-          {text: "思想政治", link: "politics/"}
-        ],
-      },
-      {
-        text: "网站搭建经验",
-        link: "webbuild/"
-      },
-      {
-        text: "计算机经验",
-        link: "computer/"
-      }
+    prefix: "culture/",
+    children:[
+      {text: "思想政治", link: "politics/"}
     ],
+  },
+  {
+    text: "网站搭建经验",
+    link: "webbuild/"
+  },
+  {
+    text: "计算机经验",
+    link: "computer/"
   }
 ]);
