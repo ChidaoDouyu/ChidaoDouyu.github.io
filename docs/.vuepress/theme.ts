1a3092b612cae764f781c3694919b630fd6b9f6f
ChidaoDouyu <3114680870@qq.com>
2025-04-10 22:02:18 +0800

    waline添加更多emoji

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -141,7 +141,8 @@ export default hopeTheme({
     comment: {
       provider: "Waline",
       serverURL: "http://waline.puremcs.top", //:8360
-      dark: "auto"
+      dark: "auto",
+      emoji: ['//unpkg.com/@waline/emojis@1.1.0/weibo','https://unpkg.com/@waline/emojis@1.2.0/alus','https://unpkg.com/@waline/emojis@1.2.0/bilibili','https://unpkg.com/@waline/emojis@1.2.0/bmoji','https://unpkg.com/@waline/emojis@1.2.0/qq','https://unpkg.com/@waline/emojis@1.2.0/tieba','https://unpkg.com/@waline/emojis@1.2.0/tw-emoji','https://unpkg.com/@waline/emojis@1.2.0/weibo','https://unpkg.com/@waline/emojis@1.2.0/soul-emoji'],
     },
 
     components: {
