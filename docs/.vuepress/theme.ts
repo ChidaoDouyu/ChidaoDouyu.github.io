ec034761c1fdb185c1ffbb1988d99ab4760d01e0
ChidaoDouyu <3114680870@qq.com>
2025-04-10 20:56:38 +0800

    localhost

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -140,7 +140,7 @@ export default hopeTheme({
     
     comment: {
       provider: "Waline",
-      serverURL: "154.64.252.155", //:8360
+      serverURL: "localhost", //:8360
       dark: "auto",
       // recaptchaV3Key: "6Lf4ZxIrAAAAAM5hr2OKHMMrNFDqdLohAINAO34O",
       reaction: ["true"],
