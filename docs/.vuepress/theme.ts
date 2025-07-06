a4f8232ce7ebca78775400cb3d5b42334ebeab6f
ChidaoDouyu <3114680870@qq.com>
2025-07-07 00:13:31 +0800

    关闭图片预览

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -116,7 +116,7 @@ export default hopeTheme({
     },
 
     // 关闭图片预览
-    // photoSwipe: false,
+    photoSwipe: false,
 
     markdownHint:{
       alert: true
@@ -148,7 +148,7 @@ export default hopeTheme({
     },
 
     components: {
-      components: ["Badge", "VPCard"],
+      components: ["Badge", "BiliBili", "PDF", "Share", "SiteInfo", "VPCard", "VidStack"],
     },
 
     // 此处开启了很多功能用于演示，你应仅保留用到的功能。
